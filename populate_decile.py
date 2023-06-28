330d4bbd1e48d807051f3fc06ff1681fa0a2aec7
Jerry Zhao <zhaojerry123@gmail.com>
2023-06-28 18:14:19 -0400

    added password reading

--- populate_decile.py
+++ populate_decile.py
@@ -1,18 +1,29 @@
 import pandas as pd
-import os, re, time
+import io, re, time
+import msoffcrypto, getpass
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 
 # Asking for user input
 xlsx_file = input("Please enter the name of the excel file:\n")
-xlsx_file = re.sub('.xlsx','', xlsx_file)
+xlsx_file = re.sub('.xlsx','', xlsx_file) #removing the .xlsx at the end of the name (if entered)
+
+# Get password
+password = getpass.getpass(prompt="Please enter the password of the file\n")
+
+decrypted_workbook = io.BytesIO()
 
 print("Opening " + xlsx_file + "...\n")
+
 try:
-    df = pd.read_excel(f'{xlsx_file}.xlsx')
+    with open(f'{xlsx_file}.xlsx', 'rb') as file:
+        office_file = msoffcrypto.OfficeFile(file)
+        office_file.load_key(password=password)
+        office_file.decrypt(decrypted_workbook)
+    df = pd.read_excel(decrypted_workbook)
 except:
-    print("Error: File not found")
+    print("Error reading file")
     exit(1)
 print(f"Read {df.shape[0]} lines!\n")
 
@@ -25,7 +36,7 @@ print(f"Found {len(addr_lst)} addresses, first address: {addr_lst[0]}\n")
 url = "https://www.neighborhoodatlas.medicine.wisc.edu/mapping"
 
 # Setting parameters for selenium to work
-path = r'/home/jzhao128/bias_autopopulator/chromedriver.exe' # Tell Annie about this download
+path = r'/home/jzhao128/bias_autopopulator/chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 options.add_argument("--remote-debugging-port=9222") # Had a "it works on my computer moment"
@@ -33,6 +44,11 @@ driver = webdriver.Chrome(path, options=options)
 driver.get(url)
 # executable_path is deprecated, use Service object instead. Will proceed with this
 
+# Click to acknowledge cookies
+cookie_btn = driver.find_element(By.ID, 'cookieconsent-dismiss')
+cookie_btn.click()
+
+# Selecting State
 state_select = Select(driver.find_element(By.NAME, 'map-state-init'))
 state_select.select_by_visible_text('Maryland')
 driver.implicitly_wait(10)
@@ -71,7 +87,6 @@ for idx, addr in enumerate(addr_lst):
         df.at[idx, 'Decile']=output
         df.at[idx, 'Percentile']=output
 df.to_excel("output.xlsx")  
-print(df)
 
 
 print("End!")
